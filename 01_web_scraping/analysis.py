40f3028f1acc6c4eb18e601257a6b758bb989aee
Pedro Gonçalves <pedrogoncalves.ie@gmail.com>
2025-04-04 17:43:32 -0300

    error handling  --v02

--- 01_web_scraping/analysis.py
+++ 01_web_scraping/analysis.py
@@ -1,26 +1,29 @@
 import pandas as pd
+import os
 
-data = pd.read_csv(
-    r'01_web_scraping\data\2025-04-03_19_43_35_product_prices.csv')
+BASE_URL = '01_web_scraping\data'
 
-# Removing duplicates
+dirs = os.listdir(BASE_URL)
 
 
 def analysis(data):
-    duplicates = data.value_counts()
     data = data.drop_duplicates().reset_index(drop=True)
 
     # Dataframe length
-    number_of_items = len(data['Product'])
+    number_of_items = len(data)
+    if 'Price' in data.columns:
+        not_found = len(data.query('Price == "Not found"'))
 
-    # Number of "Not found" results
-    not_found = data.query('Price == "Not found"')['Price'].count()
+        return round((1 - (int(not_found)/int(number_of_items))) * 100, 2)
 
-    # Perfomance result
+def executioner():
+    counter = 0    
+    for item in dirs:
+        if 'data' in item:
+            continue
+        data = pd.read_csv(f'{BASE_URL}\{item}')
+        result = analysis(data)
+        counter = counter + 1
+        print(f'{counter}/{len(dirs)-1}: Null values for file {item} ({len(data)} rows): {result}%')
 
-    result = (1 - (int(not_found)/int(number_of_items))) * 100
-
-    print(f'Current performance pct: {round(result, 2)}%')
-
-
-analysis(data)
+executioner()
--- 01_web_scraping/main.py
+++ 01_web_scraping/main.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.firefox import GeckoDriverManager
 from datetime import datetime as dt
-
+from analysis import executioner
 data = pd.read_csv('01_web_scraping/data/data.csv')
 
 options = Options()
@@ -76,3 +76,5 @@ df.to_csv(
 print("Data saved to product_prices.csv")
 
 browser.quit()
+
+executioner()
